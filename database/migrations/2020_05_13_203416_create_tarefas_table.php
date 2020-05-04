717e40d5990f191b31052bee45c3f601299111ed
Samuel B. Oldra <samuel.oldra@gmail.com>
2020-05-04 19:00:00 -0300

    CMD: php artisan make:model Tarefa -m

---
+++ database/migrations/2020_05_13_203416_create_tarefas_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTarefasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tarefas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nome');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tarefas');
+    }
+}
