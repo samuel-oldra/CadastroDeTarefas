684eb6068eee48df33ff8b43ab14c4eb351e6579
Samuel B. Oldra <samuel.oldra@gmail.com>
2020-05-04 00:00:00 -0300

    Refactoring Junho/2022

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Laravel 7 CRUD Tutorial de Exemplo</title>
+    <title>Lista de Tarefas</title>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css"/>
     <!-- Bootstrap -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return redirect("/tarefas");
 });
 
 Route::resource('tarefas', 'TarefaController');
