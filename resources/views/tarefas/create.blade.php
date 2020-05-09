2cfe06544df95d23ceb0ca940c1df46346b6c60a
Samuel B. Oldra <samuel.oldra@gmail.com>
2020-05-09 19:00:00 -0300

    Salvar tarefa 'view/controller'

---
+++ resources/views/tarefas/create.blade.php
@@ -0,0 +1,33 @@
+@extends('layout')
+
+@section('content')
+    <style>
+        .uper {
+            margin-top: 40px;
+        }
+    </style>
+    <div class="card uper">
+        <div class="card-header">
+            Adicionar Tarefa
+        </div>
+        <div class="card-body">
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div><br/>
+            @endif
+            <form method="post" action="{{ route('tarefas.store') }}">
+                <div class="form-group">
+                    @csrf
+                    <label for="nome">Nome:</label>
+                    <input type="text" class="form-control" name="nome"/>
+                </div>
+                <button type="submit" class="btn btn-primary">Adicionar</button>
+            </form>
+        </div>
+    </div>
+@endsection
