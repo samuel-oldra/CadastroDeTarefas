245bf7354e0b327e01eaa6519b4cf0c94b428720
Samuel B. Oldra <samuel.oldra@gmail.com>
2020-05-14 17:34:45 -0300

    Editar tarefa 'view/controller'

---
+++ resources/views/tarefas/edit.blade.php
@@ -0,0 +1,34 @@
+@extends('layout')
+
+@section('content')
+    <style>
+        .uper {
+            margin-top: 40px;
+        }
+    </style>
+    <div class="card uper">
+        <div class="card-header">
+            Editar Tarefa
+        </div>
+        <div class="card-body">
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div><br/>
+            @endif
+            <form method="post" action="{{ route('tarefas.update', $tarefa->id) }}">
+                @method('PATCH')
+                @csrf
+                <div class="form-group">
+                    <label for="nome">Nome:</label>
+                    <input type="text" class="form-control" name="nome" value={{ $tarefa->nome }} />
+                </div>
+                <button type="submit" class="btn btn-primary">Editar</button>
+            </form>
+        </div>
+    </div>
+@endsection
